6156532879e79a4a06e024609b10e9a88a444f6f
mylifeisskidrow@gmail.com <mylifeisskidrow@gmail.com@cdfe7f5b-dd09-552a-5caa-0de13917d04f>
2013-01-30 03:10:48 +0000

    New changelist

--- modules/mod_vhattc_tool/tmpl/filter.php
+++ modules/mod_vhattc_tool/tmpl/filter.php
@@ -7,8 +7,9 @@
  * To change this template use File | Settings | File Templates.
  */
 $link = 'index.php?option=com_k2&view=itemlist&catid=' .$params->get('extraFieldsCategory', 0);
+var_dump($filter, $params->get('extraFieldsFilter', 0));
 ?>
 <span class=""><a href="<?php echo JRoute::_($link) ?>">' .JText::_('K2_ALL') .'</a></span>
 <?php foreach ($filters as $filter) {
-    echo '<a href="' .JRoute::_($link.'&exf='.$params->get('extraFieldsFilter', 0).'&exf_val=' .$filter['value']) .'">' .$filter['name'] .'</a>';
+    echo '<a href="' .JRoute::_($link.'&exf[]='.$params->get('extraFieldsFilter', 0).'&exf_val=' .$filter['value']) .'">' .$filter['name'] .'</a>';
 } ?>
