e1a229595ab2926d6281e417d2cf046518402444
mylifeisskidrow@gmail.com <mylifeisskidrow@gmail.com@cdfe7f5b-dd09-552a-5caa-0de13917d04f>
2013-01-30 03:53:43 +0000

    New changelist

--- modules/mod_vhattc_tool/tmpl/filter.php
+++ modules/mod_vhattc_tool/tmpl/filter.php
@@ -12,10 +12,10 @@ $exf = $params->get('extraFieldsFilter', 0);
 $exf = $exf[0];
 //var_dump($filters, $params->get('extraFieldsFilter', 0)); exit;
 ?>
-<span class="fw-b"><a href="<?php echo JRoute::_($link) ?>"><?php echo JText::_('K2_ALL') ?></a></span>
+<span class="fw-b"><a href="<?php echo JRoute::_($link) ?>"><?php echo JText::_('K2_ALL') ?></a> : </span>
 <?php for($i =0, $size = sizeof($filters); $i < $size; ++$i) {
     echo '<a href="' .JRoute::_($link.'&view=itemlist&task=exfilter&catid=' .$params->get('extraFieldsCategory', 0) .'&exf[]='.$exf.'&exf_val=' .$filters[$i]['value']) .'">' .$filters[$i]['name'] .'</a>';
     if ($i < $size - 1) {
-        echo '  /  ';
+        echo '&nbsp; / &nbsp;';
     }
 } ?>
