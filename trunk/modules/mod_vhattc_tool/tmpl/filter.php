42473c2984e61094dfe2ac9af7f7acadc521fe8c
mylifeisskidrow@gmail.com <mylifeisskidrow@gmail.com@cdfe7f5b-dd09-552a-5caa-0de13917d04f>
2013-01-30 03:36:53 +0000

    New changelist

--- trunk/modules/mod_vhattc_tool/tmpl/filter.php
+++ trunk/modules/mod_vhattc_tool/tmpl/filter.php
@@ -8,11 +8,13 @@
  */
 $itemID = JRequest::getInt('Itemid');
 $link = 'index.php?option=com_k2&view=itemlist&Itemid='.$itemID;
+$exf = $params->get('extraFieldsFilter', 0);
+$exf = $exf[0];
 //var_dump($filters, $params->get('extraFieldsFilter', 0)); exit;
 ?>
 <span class="fw-b"><a href="<?php echo JRoute::_($link) ?>"><?php echo JText::_('K2_ALL') ?></a></span>
 <?php for($i =0, $size = sizeof($filters); $i < $size; ++$i) {
-    echo '<a href="' .JRoute::_($link.'&task=exfilter&catid=' .$params->get('extraFieldsCategory', 0) .'&exf[]='.$params->get('extraFieldsFilter', 0).'&exf_val=' .$filters[$i]['value']) .'">' .$filter['name'] .'</a>';
+    echo '<a href="' .JRoute::_($link.'&task=exfilter&catid=' .$params->get('extraFieldsCategory', 0) .'&exf[]='.$exf.'&exf_val=' .$filters[$i]['value']) .'">' .$filter['name'] .'</a>';
     if ($i < $size - 1) {
         echo '  /  ';
     }
