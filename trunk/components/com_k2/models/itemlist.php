56823912bbac3ea7f332a9c44ca99fad2ba19a0b
mylifeisskidrow@gmail.com <mylifeisskidrow@gmail.com@cdfe7f5b-dd09-552a-5caa-0de13917d04f>
2013-01-30 04:20:25 +0000

    New changelist

--- trunk/components/com_k2/models/itemlist.php
+++ trunk/components/com_k2/models/itemlist.php
@@ -234,6 +234,7 @@ class K2ModelItemlist extends K2Model
                 $year = JRequest::getInt('y');
                 $day = JRequest::getInt('d');
                 $catid = JRequest::getVar('catid');
+                $catid = (array) $catid;
                 $exf = JRequest::getVar('exf');
                 $exf_value = JRequest::getVar('exf_val');
                 if (!empty($catid)) {
